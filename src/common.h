15eb0c1511e9d0e311e1a4f95485c136f985bce5
Simone Cappabianca <s.cappabianca@gmail.com>
2023-11-08 22:14:30 +0100

    Refactor of common.h

--- src/common.h
+++ src/common.h
@@ -2,6 +2,8 @@
 /* common.h                                                                   */
 /* -------------------------------------------------------------------------- */
 #define DEBUG
+#define PATH_SOCKET "./sockets/"
+#define EXT_SOCKET ".sck"
 
 /* -------------------------------------------------------------------------- */
 /* Macro - LOG                                                                */
@@ -10,12 +12,21 @@
 #define PREFIX_LEN_LOG 25                            /* Length of log-prefix  */
 
 /* -------------------------------------------------------------------------- */
-/* Macro - HMI                                                                */
+/* Macro - HMI INPUT                                                          */
 /* -------------------------------------------------------------------------- */
 #define HMI_INPUT_SOCKET "hmi_input"
 #define HMI_INPUT_MSG_LEN 10
 #define HMI_INPUT_COMMAND_START "INIZIO"
 #define HMI_INPUT_COMMAND_PARKING "PARCHEGGIO"
 #define HMI_INPUT_COMMAND_STOP "ARRESTO"
+
+/* -------------------------------------------------------------------------- */
+/* Macro - HMI OUTPUT                                                         */
+/* -------------------------------------------------------------------------- */
 #define HMI_OUTPUT_SOCKET "hmi_output"
 #define HMI_OUTPUT_MSG_LEM 10
+
+/* -------------------------------------------------------------------------- */
+/* Macro - FRONT WISHSHIELD CAMERA                                            */
+/* -------------------------------------------------------------------------- */
+#define FWC_LOG_FILE_NAME "camera"
