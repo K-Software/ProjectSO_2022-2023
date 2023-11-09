287c357edb1aca8dd3b1b6a542147f027954475e
Simone Cappabianca <s.cappabianca@gmail.com>
2023-11-09 23:07:19 +0100

    Steer by wire component WIP

---
+++ src/steer_by_wire.c
@@ -0,0 +1,71 @@
+/* -------------------------------------------------------------------------- */
+/* steer_by_wire.c                                                            */
+/* -------------------------------------------------------------------------- */
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h> /* For AF_UNIX sockets */
+#include <unistd.h>
+#include "common.h"
+#include "log.h"
+#include "steer_by_wire.h"
+
+/* -------------------------------------------------------------------------- */
+/* Macro                                                                      */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/* Functions                                                                  */
+/* -------------------------------------------------------------------------- */
+
+#ifdef DEBUG
+void main(void)
+{
+    steerByWireStart();
+} 
+#endif
+
+void steerByWireStart(void) 
+{
+    int fd, esito;
+    char command[SBW_MSG_LEN];
+
+    char *socketName = malloc(strlen(PATH_SOCKET)+strlen(SBW_LOG_FILE_NAME)+strlen(EXT_SOCKET)+1);
+    strcpy(socketName, PATH_SOCKET);
+    strcat(socketName, SBW_LOG_FILE_NAME);
+    strcat(socketName, EXT_SOCKET);
+
+    do {
+        addLog(SBW_LOG_FILE_NAME, "Apro il FIFO");
+        fd = open(socketName,O_RDONLY | O_NONBLOCK);
+        if (fd == -1) {
+            addLog(SBW_LOG_FILE_NAME, "Errore nell'apertura del FIFO");
+            sleep(15);
+        } else {
+            addLog(SBW_LOG_FILE_NAME, "Aperto il FIFO");
+        }
+    } while (fd == -1);
+    free(socketName);
+
+    while(1) {
+        if ((esito = read(fd, command, sizeof(command))) != 0) {
+            if (strcmp(command, ECU_COMMAND_RIGHT) == 0) {
+                addLog(SBW_LOG_FILE_NAME, SBW_TURN_RIGHT_LOG_MSG);
+            } else if (strcmp(command, ECU_COMMAND_LEFT) == 0) {
+                addLog(SBW_LOG_FILE_NAME, SBW_TURN_LEFT_LOG_MSG);
+            } else {
+                addLog(SBW_LOG_FILE_NAME, LOG_MSG_WRONG_COMMAND);
+            }
+            sleep(4);
+        } else {
+            addLog(SBW_LOG_FILE_NAME, LOG_MSG_NO_ACTION);
+            sleep(1);
+        }
+    }
+}
