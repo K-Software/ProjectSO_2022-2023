69e52e47f7f92aa3ba8219492d4790605ca3310a
Simone Cappabianca <s.cappabianca@gmail.com>
2023-11-09 23:11:59 +0100

    Updated common.h

--- src/common.h
+++ src/common.h
@@ -1,9 +1,11 @@
 /* -------------------------------------------------------------------------- */
 /* common.h                                                                   */
 /* -------------------------------------------------------------------------- */
-#define DEBUG
+#define DEBUGXXX
 #define PATH_SOCKET "./sockets/"
 #define EXT_SOCKET ".sck"
+#define LOG_MSG_WRONG_COMMAND "COMANDO NON PREVISTO"
+#define LOG_MSG_NO_ACTION "NO ACTION"
 
 /* -------------------------------------------------------------------------- */
 /* Macro - LOG                                                                */
@@ -27,6 +29,39 @@
 #define HMI_OUTPUT_MSG_LEM 10
 
 /* -------------------------------------------------------------------------- */
+/* Macro - ECU                                                                */
+/* -------------------------------------------------------------------------- */
+#define ECU_COMMAND_LEFT "SINISTRA"
+#define ECU_COMMAND_RIGHT "DESTRA"
+#define ECU_COMMAND_THROTTLE "INCREMENTO 5"
+#define ECU_COMMAND_BRAKE "FRENO 5"
+
+/* -------------------------------------------------------------------------- */
 /* Macro - FRONT WISHSHIELD CAMERA                                            */
 /* -------------------------------------------------------------------------- */
 #define FWC_LOG_FILE_NAME "camera"
+
+/* -------------------------------------------------------------------------- */
+/* Macro - STEER BY WIRE                                                      */
+/* -------------------------------------------------------------------------- */
+#define SBW_LOG_FILE_NAME "steer"
+#define SBW_SOCKET "steer_by_wire"
+#define SBW_MSG_LEN 10
+#define SBW_TURN_LEFT_LOG_MSG "STO GIRANDO A SINISTRA"
+#define SBW_TURN_RIGHT_LOG_MSG "STO GIRANDO A DESTRA"
+
+/* -------------------------------------------------------------------------- */
+/* Macro - THROTTLE CONTROL                                                   */
+/* -------------------------------------------------------------------------- */
+#define TC_LOG_FILE_NAME "throttle"
+#define TC_SOCKET "throttle_control"
+#define TC_MSG_LEN 13
+#define TC_LOG_MSG "AUMENTO 5"
+
+/* -------------------------------------------------------------------------- */
+/* Macro - BRAKE BY WIRE                                                      */
+/* -------------------------------------------------------------------------- */
+#define BBW_LOG_FILE_NAME "brake"
+#define BBW_SOCKET "brake_by_wire"
+#define BBW_MSG_LEN 8
+#define BBW_LOG_MSG "FRENO 5"
