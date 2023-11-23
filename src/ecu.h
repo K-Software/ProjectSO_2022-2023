a748913fb1d089764554ad299bcb84ab087fb790
Simone Cappabianca <s.cappabianca@gmail.com>
2023-11-24 00:22:03 +0100

    ECU component WIP

--- src/ecu.h
+++ src/ecu.h
@@ -3,7 +3,5 @@
 /* ---------------------------------------------------------------------------*/
 void ecuStart(void);
 void initSockets(void);
-void sendDataToSBWComponent(char *socketName, char *command);
-void sendDataToTCComponent(char *socketName);
-void sendDataToBBWComponent(char *socketName);
+void sendDataToComponent(char *socketName, char *command);
 void parking(int *speed, pid_t pids[4]);
